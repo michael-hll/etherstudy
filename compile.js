06dd8d110532c177568c8b25c158fa8f98f48aa0
michael <michael_hll@hotmail.com>
2024-02-01 10:16:10 +0800

    init Inbox contract

---
+++ compile.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+// Read 'Inbox.sol' file from the 'contracts' folder
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+// settings for building
+let input = {
+    language: 'Solidity',
+    sources: {
+        'Inbox.sol': {
+            content: source
+        }
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['*']
+            }
+        }
+    }
+};
+
+// start build and get the build output
+let output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+// write output to json file
+
+fs.writeFile(
+  inboxPath + '.json',
+  JSON.stringify(output, null, 2),
+  {
+    encoding: "utf8",
+    flag: "w",
+    mode: 0o666
+  },
+  (err) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("File written successfully\n");      
+    }
+  });
